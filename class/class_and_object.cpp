c71056c7a5c4a85b3bd29f678c207d378bace9d7
kelinci dev <61102653+kelincidev@users.noreply.github.com>
2021-04-22 15:20:07 -0700

    Update class_and_object.cpp

--- class/class_and_object.cpp
+++ class/class_and_object.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+using namespace std;
+
+class ayam {
+	public:
+		string warna;
+		ayam(){}
+		void coklat(){ cout<<"ayam coklat\n"; }
+		void putih();
+		void bedawarna(){ cout<<"ayam "<<warna<<endl; }
+		~ayam(){}
+};
+
+void ayam::putih() {
+	cout<<"ayam putih";
+}
 
 main() {
-  cout<<"test 1";
+	ayam Ayam;
+	Ayam.coklat();
+	Ayam.warna = "hitam";
+	Ayam.bedawarna();
 }
